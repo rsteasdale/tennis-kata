1968b7c84a08f703d9ba6bcd361caf70aa66b8c2
Richard Teasdale <rsteasdale@gmail.com>
2020-06-24 10:06:23 +0100

    Test to validate player 1 winning first point

--- Tennis/Tests.cs
+++ Tennis/Tests.cs
@@ -13,13 +13,36 @@ namespace Tennis
 
             Assert.Equal("Love-Love", game.Score());
         }
+
+        [Fact]
+        public void GivenPlayer1WinsFirstPointThenUpdateScore()
+        {
+            var game = new Game();
+            game.WinPoint("Player 1");
+
+            Assert.Equal("15-Love", game.Score());
+        }
     }
 
     public class Game
     {
+        private readonly string _player1;
+        private string _score = "Love-Love";
+
+        public Game(string player1 = "Player 1")
+        {
+            _player1 = player1;
+        }
+
         public string Score()
         {
-            return "Love-Love";
+            return _score;
+        }
+
+        public void WinPoint(string player)
+        {
+            if(player == _player1)
+                _score = "15-Love";
         }
     }
 }
